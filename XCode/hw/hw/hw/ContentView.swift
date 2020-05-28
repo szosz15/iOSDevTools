23ba2576c2288ff465b30712873439cdd5b1fa8a
jakubszostok <jakubszostok@gmail.com>
2020-05-28 15:08:44 +0200

    #1 Projekt XCode

---
+++ XCode/hw/hw/hw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw
+//
+//  Created by Kornel on 26/05/2020.
+//  Copyright © 2020 Kornel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
